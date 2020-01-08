66db83edc090640c8da809fc99eae3a930857a4b
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2020-01-08 12:54:10 -0500

    added nav links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import logo from '../../img/manatreeservice.jpg';
 
@@ -11,6 +13,9 @@ const useStyles = makeStyles(theme => ({
   logo: {
     maxWidth: '20%',
     maxHeight: '20%'
+  },
+  button: {
+    margin: theme.spacing(2)
   }
 }));
 
@@ -21,8 +26,22 @@ const Navbar = () => {
     <AppBar>
       <Toolbar>
         <a href='/'>
-          <img src={logo} alt='logo' className={classes.logo} />
+          <div>
+            <img src={logo} alt='logo' className={classes.logo} />
+          </div>
         </a>
+        <Button className={classes.button}>
+          <Link href='#home'>Home</Link>
+        </Button>
+        <Button className={classes.button}>
+          <Link href='#company'>Company</Link>
+        </Button>
+        <Button className={classes.button}>
+          <Link href='#services'>Services</Link>
+        </Button>
+        <Button className={classes.button}>
+          <Link href='#contact'>Contact</Link>
+        </Button>
       </Toolbar>
     </AppBar>
   );
