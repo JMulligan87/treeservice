35aec207f93b0c7ea650a5c825de64a12fbb43cc
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2020-01-09 12:10:45 -0500

    added a clickable phone number

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,36 +1,32 @@
 import React from 'react';
 import tree from '../../img/cut.jpg';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import { Typography, Grid, Link } from '@material-ui/core';
 
 const Home = () => {
   return (
-    <div className='wrapper'>
-      <Grid mb=''>
-        <img
-          src={tree}
-          alt='tree service'
-          style={{
-            //   position: 'absolute',
-            height: '100%',
-            width: '100%'
-          }}
-        />
-      </Grid>
-
-      <Grid wrapper='true' style={{ margin: '50px 150px 50px 150px' }}>
+    <Grid container>
+      <img
+        src={tree}
+        alt='tree service'
+        style={{
+          position: 'relative',
+          height: '100%',
+          width: '100%'
+        }}
+      />
+      <Grid style={{ margin: '50px 150px 50px 150px' }}>
         <Typography variant='h2' align='center'>
           In business since the 1900's.
         </Typography>
         <br></br>
-        <Typography paragraph='true'>
+        <Typography paragraph>
           Manahawkin Tree Service, a family owned and operated company takes
           great pride in the work we do. Our customers are extremely important
           to us, and we strive to keep each and every customer a customer for
           life. We will listen to your needs, do the work you request, and help
           turn your property into the yard you always dreamed of.
         </Typography>
-        <Typography paragraph='true'>
+        <Typography paragraph>
           Tree and shrub removal and/or maintenance can be difficult and
           demanding. It requires specific tools and operators trained in the use
           of these tools. Many jobs can be too dangerous or too difficult for
@@ -39,8 +35,23 @@ const Home = () => {
           while maintaining an excellent record of safety. All jobs are owner
           directed and supervised.
         </Typography>
+        <Typography paragraph align='center'>
+          Call us now and see what we can do to make your yard a showplace!
+        </Typography>
+        <Typography align='center'>
+          <Link
+            href='tel:6095978846'
+            style={{
+              backgroundColor: 'white',
+              //   border: '1px solid black',
+              fontSize: '30px'
+            }}
+          >
+            609-597-8846
+          </Link>
+        </Typography>
       </Grid>
-    </div>
+    </Grid>
   );
 };
 
