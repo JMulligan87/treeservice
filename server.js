99cfc87fee10b9fc84479c79384a011f5e730a46
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2020-01-15 23:27:54 -0500

    contact form now sends an email

--- server.js
+++ server.js
@@ -1,38 +1,57 @@
-// const nodemailer = require('nodemailer');
-// require('dotenv').config();
-
-// let transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD
-//   }
-// });
-
-// let mailOptions = {
-//   from: 'dadtothemax87@gmail.com',
-//   to: '87jmulligan@gmail.com',
-//   subject: 'Contact from the website',
-//   text: 'It works'
-// };
-
-// transporter.sendMail(mailOptions, function(err, data) {
-//   if (err) {
-//     console.log('Error occurred');
-//   } else {
-//     console.log('Email sent!');
-//   }
-// });
 const express = require('express');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json({ extended: false }));
 
-const { sendEmail } = require('./mail/mail');
-
 app.post('/api/form', (req, res) => {
   console.log(req.body);
-  sendEmail(req.body.email, req.body.name);
+
+  const output = ` 
+              <br />
+              <br />
+              <div>Name: ${req.body.name} </div>
+              <br />
+              <div>Phone: ${req.body.phone} </div>
+              <br />
+              <div>Address: ${req.body.address} </div>
+              <br />
+              <div>Message: ${req.body.message} </div>
+              <br />
+              <br />
+  `;
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL, // generated ethereal user
+      pass: process.env.PASSWORD // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: process.env.EMAIL, // sender address
+    to: '87jmulligan@gmail.com', // list of receivers
+    subject: 'Contact from website', // Subject line
+    text: 'Hello world?', // plain text body
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.render('contact', { msg: 'Email has been sent' });
+  });
 });
 
 const PORT = process.env.PORT || 5000;
