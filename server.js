4d6c1beab62dbe6a8d458e72c3355f95289f69e3
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2020-10-08 14:31:04 -0400

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
@@ -69,6 +69,16 @@ app.post('/api/form', (req, res) => {
     res.render('contact', { msg: 'Email has been sent' });
   });
 });
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
